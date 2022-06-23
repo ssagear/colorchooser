e09b87dec77abba446b9baf3699ff578ce8af139
Sheila Sagear <ssagear@ufl.edu>
2022-06-23 11:40:07 -0700

    fixed double quotes in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(
-name=”colorchooser”,
-version=”0.1”,
-packages=find_packages()
-)
+setup(name='colorchooser',
+version='1.0',
+packages=find_packages())
