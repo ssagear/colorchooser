919dac09b71dfc305051b16538cb1a882e9b12f1
Rebecca Lewis <rebeccalewis@sorcessiancomlink-2.local>
2022-06-24 16:38:21 -0700

    Added docstrings

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(name='plotpainter',
-<<<<<<< HEAD
-version='0.0.0',
-=======
-version='1.0',
->>>>>>> 893ea136005fe2b5fb5c16d137450c9561d3bbba
+version='0.0.1',
 packages=find_packages())
