a128a6e0ad3fc1ef5c84921f4aa5c605a6e0e88d
RileyAnne Sharpe <rhsharpe@crimson.ua.edu>
2022-06-24 16:15:23 -0700

    started end to end testing-didn't finish

---
+++ test/end-to-end-tests/end_to_end.py
@@ -0,0 +1,12 @@
+import plotpainter as p
+import matplotlib.pyplot as plt
+import PySimpleGUI as sg
+
+def test_plotpainter():
+    """Tests that plotpainter produces a GUI that allows the user to 
+    choose the colors of different artists in a figure.
+    """
+
+
+
+    return("Test passed!")
