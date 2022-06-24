893ea136005fe2b5fb5c16d137450c9561d3bbba
Sheila Sagear <ssagear@ufl.edu>
2022-06-24 14:54:52 -0700

    Changed readme and setup name

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../plotpainter/'))
 
+import plotpainter
+
 
 # -- Project information -----------------------------------------------------
 
